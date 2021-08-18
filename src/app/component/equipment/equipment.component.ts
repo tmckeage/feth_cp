1f8f55f0c7e76efdbe18b44008e1db95dbba5968
Nitin <nitinp@alohatechnologydev.com>
2021-08-18 13:54:48 +0530

    equipment changes, amplify service, forgot password code

--- src/app/component/equipment/equipment.component.ts
+++ src/app/component/equipment/equipment.component.ts
@@ -18,13 +18,15 @@ export class EquipmentComponent implements OnInit {
 	scannerForms!: FormGroup;
 	columnDefs: any;
 	rowData: any;
+	isModalCloseFlag: any;
+	room: any;
 	constructor(private modalService: NgbModal) { }
 	ngOnInit(): void {
 		this.scannerForms = new FormGroup({
 			"make": new FormControl('Select', [Validators.required]),
 			"model": new FormControl('Select', [Validators.required]),
-			"room": new FormControl('Select', [Validators.required]),
-			"facility": new FormControl('Select', [Validators.required]),
+			"room": new FormControl('Select', []),
+			"facility": new FormControl('Select', []),
 			"sn": new FormControl('', [Validators.required])
 		});
 
@@ -54,13 +56,6 @@ export class EquipmentComponent implements OnInit {
 				]
 			}
 		];
-
-		// this.scanners = [
-		// 					{ name: "GE Logiq: L12345678", cirs: "CIRS", rm: "RM 1", due: "7/31/21" },
-		// 					{ name: "Philips: M12345678", cirs: "CIRS", rm: "RM 2", due: "7/31/21" }
-		// 				];
-		// this.scanner = [{ name: "Philips: M12345678", cirs: "CIRS", rm: "RM 2", due: "7/31/21" }];
-		this.equipment = [{ scan: 'C26:L1q234', scan1: 'L412:M1234', scan2: 'E27:L1234' }];
 
 		// This is temp JSON, once API's gets integrate then this will be get remove
 		this.columnDefs = [
@@ -100,7 +95,11 @@ export class EquipmentComponent implements OnInit {
 		this.modalService.open(view, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
 			this.closeResult = `Closed with: ${result}`;
 		});
+		this.isModalCloseFlag = false;
+
 	}
+
+	
 	// Edit modal
 	onEdit($event: any, content: any) {
 		this.showModalTitle = 'Edit Scanner';
@@ -111,6 +110,7 @@ export class EquipmentComponent implements OnInit {
 		this.setFacility(1);
 		this.setRoom(1);
 		this.setSN(sn);
+		this.modalService.dismissAll();
 		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
 			this.closeResult = `Closed with: ${result}`;
 		});
@@ -120,4 +120,5 @@ export class EquipmentComponent implements OnInit {
 	setSN(inputVal: any) { this.scannerForms.controls.sn.setValue(inputVal) }
 	setRoom(inputVal: any) { this.scannerForms.controls.room.setValue(inputVal) }
 	setFacility(inputVal: any) { this.scannerForms.controls.facility.setValue(inputVal) }
+	get scannerForm() { return this.scannerForms.controls; }
 }
