5e8b40d212bf09fe9e2e644c5e1391cdc07c38fd
Nitin <nitinp@alohatechnologydev.com>
2021-11-08 20:48:35 +0530

    horizontal distance table in empty value on added dash

--- src/app/component/review/horizontal-distance/horizontal-distance.component.ts
+++ src/app/component/review/horizontal-distance/horizontal-distance.component.ts
@@ -10,15 +10,15 @@ export class HorizontalDistanceComponent implements OnInit {
   transducer: any;
   datalist: any[] = [];
   measurementList: any;
-  horizontal_distance:any[][] = [[],[]];
-  date : any;
+  horizontal_distance: any[] = [];
+  date: any;
   unitName: any;
   horizontal_R_One: any;
   horizontal_R_Two: any;
   constructor(private equipment: EquipmentService) { }
 
   ngOnInit(): void {
-    this.datalist =  this.equipment.getScanner();
+    this.datalist = this.equipment.getScanner();
     this.datalist.forEach((res: any) => {
       this.date = res.last_study.date_performed;
       let data = res.transducers;
@@ -30,15 +30,17 @@ export class HorizontalDistanceComponent implements OnInit {
         Object.entries(imgList).forEach(([k, v]) => {
           items.push({
             name: k,
-            value:v
+            value: v
           });
-          items.forEach((data:any) => {
-            if (data.name == 'horizontal_distance') { 
-              let result = data.value;
-               this.horizontal_distance =  Object.values(result);
-                console.log(this.horizontal_distance);
+          items.forEach((data: any) => {
+            if (data.name == 'horizontal_distance') {
+              this.horizontal_distance = Object.values(data.value);
+              // let result = data.value.row_one;
+              //   result.forEach((res: any) => {
+           
+              // });
             }
-         });
+          });
         });
       });
     });
