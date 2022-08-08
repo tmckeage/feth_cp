338f95fcadaa30cdd58d2020a498ee77dec0ba8a
Nitin <nitinp@alohatechnologydev.com>
2022-08-08 01:18:26 -0700

    Env changes

---
+++ src/app/modules/testObject.ts
@@ -0,0 +1,25 @@
+
+export class TrstObject {
+    
+    // let equipments: any[] = [
+    //     { model: "Sparq", barcode: "93847641725", organization: "CIRS", facility: "CIRS", user: "tester" }
+    // ];
+    
+//     let transducerList = [
+//         {
+//             barcode: "90555471514",
+//             last_study: null,
+//             make: "Philips",
+//             model: "L12-4",
+//             next_Study_Due: { date: "2022-02-09T08:10:53.544Z", type: "acceptance" },
+//             date: "2022-02-09T08:10:53.544Z",
+//             type: "acceptance",
+//             organization: "CIRS",
+//             scanner_id: "0iAZc35Xxhze_m9ZLInW2g",
+//             serial_number: "453561652583",
+//             transducer_id: "WUzIKYb3mFxNv0zLEK4eLg",
+//             analysis_parameters: {},
+//             user: "tester"
+//         }
+//     ]
+// }
