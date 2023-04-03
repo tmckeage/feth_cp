90ceca1b5ee78c7290cbc446e74a55e0a69691bc
tmckeagecirs <60272977+tmckeagecirs@users.noreply.github.com>
2023-04-03 19:20:40 -0400

    Update environment.ts
    revert back to old url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url :'https://9vhgbajlm0.execute-api.us-east-1.amazonaws.com/dev',
+  api_url :'https://krf3bhq6zf.execute-api.us-east-1.amazonaws.com/qa',
   api_study_list: 'https://s3.amazonaws.com/com.cirsinc.jsonfiles/C5-2R+3.5Mhz.dcm_results_new.json'
 };
 
