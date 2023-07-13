b220fb7fb4ea75240f36c76e04d5b27fdbb3965c
pradips <pradips@alohatechnology.com>
2023-07-13 15:18:35 +0530

    Refactor code for UQA-22, UQA-23, UQA-24

--- src/app/component/evaluations/evalution-review/scanner-evaluation-review/scanner-evaluation-review.component.ts
+++ src/app/component/evaluations/evalution-review/scanner-evaluation-review/scanner-evaluation-review.component.ts
@@ -17,7 +17,6 @@ export class ScannerEvaluationReviewComponent implements OnInit {
     scannerDataEditNote: string = '';
     basicLuminance: any;
     luminanceUniformity: any;
-    luminanceResponseData: any;
     
     displayPerformanceTitle:any [] = [];
     
@@ -30,52 +29,53 @@ export class ScannerEvaluationReviewComponent implements OnInit {
         // console.log(this.scannerData);
         
         // Basic Luminance
-        if(this.scannerData?.display_performance?.luminance){
-            let luminance = this.scannerData?.display_performance?.luminance;
-            
-            if(parseFloat(luminance.ambient) >= 0 && parseFloat(luminance[1]) >= 0 && parseFloat(luminance[18]) > 0){
-                let ambianceRatio = (parseFloat(luminance['ambient']) / parseFloat(luminance[1])).toFixed(3); // A Value
-                let lMin = parseFloat(luminance['ambient']) + parseFloat(luminance[1]); // C Value
-                let lMax = parseFloat(luminance['ambient']) + parseFloat(luminance[18]); // D Value
-                let luminanceRatio = Math.round(lMax/lMin); // B Value
+        if(this.scannerData?.display_performance?.luminance?.basic){
+            let luminance = this.scannerData?.display_performance?.luminance?.basic;
+            if(parseFloat(luminance?.ambient) >= 0 && parseFloat(luminance?.gs_0) >= 0 && parseFloat(luminance?.gs_255) > 0){
+                let lMin = parseFloat(luminance.ambient) + parseFloat(luminance.gs_0); // C Value
+                let lMax = parseFloat(luminance.ambient) + parseFloat(luminance.gs_255); // D Value
                 
-                this.basicLuminance = { 
-                    ambianceRatio: ambianceRatio, 
-                    luminanceRatio: luminanceRatio, 
-                    lMin: lMin, 
-                    lMax: lMax,
-                    assessment: 'pass',
-                };
+                if(parseFloat(luminance?.ambient) !== 0){
+                    luminance.ambianceRatio = (parseFloat(luminance.ambient) / parseFloat(luminance.gs_0)).toFixed(3); // A Value
+                } else {
+                    luminance.ambianceRatio = 'Undefined'; // A Value
+                }
+                luminance.luminanceRatio = Math.round(lMax/lMin); // B Value
+                luminance.lMin = lMin.toFixed(3);
+                luminance.lMax = lMax.toFixed(3);
             }
         }
         
         // Luminance Uniformity
-        if(this.scannerData?.display_performance?.test_pattern && this.scannerData?.display_performance?.luminance){
-            let ambient = parseFloat(this.scannerData?.display_performance?.luminance?.ambient);
-            let testPattern = this.scannerData?.display_performance?.test_pattern;
+        if(this.scannerData?.display_performance?.luminance?.basic?.ambient && this.scannerData?.display_performance?.luminance?.uniformity){
+            let ambient = parseFloat(this.scannerData?.display_performance?.luminance?.basic?.ambient);
+            let uniformity = this.scannerData?.display_performance?.luminance?.uniformity;
             let testPatternValues = true;
             
             if(ambient >= 0){
-                Object.keys(testPattern).forEach( (key, index) => {
-                    
-                    testPattern[key] = parseFloat(testPattern[key]);
-                    if( testPattern[key] <= 0 && testPatternValues){
-                        testPatternValues = false;
+                Object.keys(uniformity).forEach( (key, index) => {
+                    if(key !== 'assessment'){
+                        if(uniformity[key]){
+                            if( parseFloat(uniformity[key]) <= 0 && testPatternValues){
+                                testPatternValues = false;
+                            }
+                        } else {
+                            testPatternValues = false;
+                        }
                     }
-                })
+                });
                 
-                if(ambient > 0 && testPatternValues){
-                    let arrayOfTestPattern:any[] = Object.values(testPattern);
+                if(testPatternValues){
+                    let arrayOfTestPattern:any[] = [];
+                    Object.keys(uniformity).forEach( (key, index) => {
+                        if(key !== 'assessment'){
+                            arrayOfTestPattern.push(parseFloat(uniformity[key]));
+                        }
+                    });
+                    
                     let max = Math.max(...arrayOfTestPattern);
                     let min = Math.min(...arrayOfTestPattern);
-                    let maximumDeviation = (200 * (max - min) / (max + min + 2 * ambient)).toFixed(0);
-                    
-                    this.luminanceUniformity = { 
-                        ambient: ambient,
-                        testPattern: testPattern, 
-                        maximumDeviation: maximumDeviation, 
-                        assessment: 'pass',
-                    };
+                    uniformity.maximumDeviation = (200 * (max - min) / (max + min + 2 * ambient)).toFixed(0);
                 }
             }
         }
@@ -127,52 +127,46 @@ export class ScannerEvaluationReviewComponent implements OnInit {
                 data: this.scannerData?.display_performance?.qualitative,
             },
             {
-                layout: this.basicLuminance ? 'basicLuminance' : this.basicLuminance,
-                title: this.basicLuminance ? 'Basic Luminance' : this.basicLuminance,
-                parentCategory: this.basicLuminance ? 'display_performance' : this.basicLuminance,
-                category: this.basicLuminance ? 'luminance' : this.basicLuminance,
-                data: this.basicLuminance
+                layout: 'basicLuminance',
+                title: 'Basic Luminance',
+                parentCategory: 'display_performance',
+                category: 'luminance_basic',
+                data: this.scannerData?.display_performance?.luminance?.basic
             },
             {
                 layout: 'luminanceResponse',
                 title: 'Luminance Response',
-                parentCategory: 'luminanceResponse',
-                category: 'luminanceResponse',
-                data: this.luminanceResponseData
+                parentCategory: 'display_performance',
+                category: 'luminance_full',
+                data: this.scannerData?.display_performance?.luminance?.full
             },
             {
-                layout: this.luminanceUniformity ? 'luminanceUniformity' : this.luminanceUniformity,
-                title: this.luminanceUniformity ? 'Luminance Uniformity' : this.luminanceUniformity,
-                parentCategory: this.luminanceUniformity ? 'display_performance' : this.luminanceUniformity,
-                category: this.luminanceUniformity ? 'test_pattern' : this.luminanceUniformity,
-                data: this.luminanceUniformity
+                layout: 'luminanceUniformity',
+                title: 'Luminance Uniformity',
+                parentCategory: 'display_performance',
+                category: 'luminance_uniformity',
+                ambient: this.scannerData?.display_performance?.luminance?.basic?.ambient,
+                data: this.scannerData?.display_performance?.luminance?.uniformity
             },
         ];
         
     }
     
-
+    
     
     async luminanceResponse(luminanceResponse: any){
-        if(luminanceResponse){
-            this.luminanceResponseData = {
-                assessment: 'pass',
-            }
+        if(luminanceResponse?.basic?.ambient && luminanceResponse?.full){
             
-            let data = luminanceResponse;      
+            let data = luminanceResponse?.full;      
             let meas_luminance:any [] = []; // this value come from API
-            let ambient:any; // this value come from API
             let greyLevel:any = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255]; // In future that will get replce with API
             
             await Object.keys(data).forEach( key => {
-                if(key !== "ambient") {
-                    data[key] = parseFloat(data[key]);
-                    meas_luminance.push(data[key]);
-                } else if(key === "ambient") {
-                    ambient = parseFloat(data[key]);
+                if(key !== "assessment") {
+                    meas_luminance.push(parseFloat(data[key]));
                 }
             }); 
-            this.buildDemo(meas_luminance, greyLevel, ambient);
+            this.buildDemo(meas_luminance, greyLevel, parseFloat(luminanceResponse?.basic?.ambient));
         }
     }
     
@@ -481,7 +475,7 @@ export class ScannerEvaluationReviewComponent implements OnInit {
     }
     
     
-       
+    
     ascOrderObject = (a:any, b:any) =>{
         if(a.key > b.key) return b.key;
     }
@@ -490,10 +484,18 @@ export class ScannerEvaluationReviewComponent implements OnInit {
         const data = { assessment: assessmentValue };
         const parentCategory = equDetails?.value?.parentCategory || equDetails?.parentCategory;
         const category = equDetails?.value?.category || equDetails?.category; 
-               
+        
         this.evaluationsService.scannerAssesment(scannerId, category, data).subscribe(
             value => { if(value.scanner_id){
-                this.scannerData[parentCategory][category].assessment = assessmentValue;
+                if(category === 'luminance_basic'){
+                    this.scannerData[parentCategory].luminance.basic.assessment = assessmentValue;
+                } else if(category === 'luminance_full') {
+                    this.scannerData[parentCategory].luminance.full.assessment = assessmentValue;
+                } else if(category === 'luminance_uniformity') {
+                    this.scannerData[parentCategory].luminance.uniformity.assessment = assessmentValue;
+                } else {
+                    this.scannerData[parentCategory][category].assessment = assessmentValue;
+                }
                 this.toastr.success('Assessment Updated successfully', '', { timeOut: 1500});
             } else {
                 this.toastr.error('Please try again', '', { timeOut: 1500});
